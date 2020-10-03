3398b113be67dc3b20587dd5e83e16572a0cfe70
joejoe123312 <joelcenteno123312@gmail.com>
2020-10-03 11:14:13 +0800

    testing file download

--- application/views/AddResearch.php
+++ application/views/AddResearch.php
@@ -1,7 +1,10 @@
 <div class="section-body contain-lg">
-<div class="col-lg-12">
-    <center class="style-default-dark">		
-    <h1 style="padding:20px;font-weight:bold">Welcome To Dashboard</h1>	</center>							</div><!--end .col -->
+    <div class="col-lg-12">
+        <center class="style-default-dark">
+            <h1 style="padding:20px;font-weight:bold">Welcome To Dashboard</h1>
+        </center>
+    </div>
+    <!--end .col -->
 </div>
 
 <?php $addProjClassification = base_url() . "AddResearch/projectClassification" ?>
@@ -9,13 +12,13 @@
 <?php $home = base_url() . "Dashboard" ?></a>
 
 
-<a href="<?=$addProjClassification ?>"><button type="button" class="btn ink-reaction btn-raised btn-primary">Add Project Classification</button></a>
-<a href="<?=$addResearch ?>"><button type="button" class="btn ink-reaction btn-raised btn-primary">Add New Research</button></a>
-<a href="<?=$home ?>"><button type="button" class="btn ink-reaction btn-raised btn-primary">Home</button></a>
+<a href="<?= $addProjClassification ?>"><button type="button" class="btn ink-reaction btn-raised btn-primary">Add Project Classification</button></a>
+<a href="<?= $addResearch ?>"><button type="button" class="btn ink-reaction btn-raised btn-primary">Add New Research</button></a>
+<a href="<?= $home ?>"><button type="button" class="btn ink-reaction btn-raised btn-primary">Home</button></a>
 <div style="margin-bottom:20px"></div>
 
 <div class="row">
-    
+
 </div>
 <!--end .row -->
 <div class="row">
@@ -24,38 +27,38 @@
         <div class="data table-responsive">
             <table id="datatable1" class="table table-striped table-hover">
                 <thead>
-                    
-                        <th class="sort-numeric">#</th>
-                        <th class="sort-alpha">Project Title</th>
-                        <th class="sort-alpha">Academic Year</th>
-                        <th class="sort-alpha">Project Classification</th>
-                        <th class="sort-alpha">Researchers</th>
-                        <th class="sort-alpha">File</th>
-                        <th>Options</th>
-                    
+
+                    <th class="sort-numeric">#</th>
+                    <th class="sort-alpha">Project Title</th>
+                    <th class="sort-alpha">Academic Year</th>
+                    <th class="sort-alpha">Project Classification</th>
+                    <th class="sort-alpha">Researchers</th>
+                    <th class="sort-alpha">File</th>
+                    <th>Options</th>
+
                 </thead>
                 <tbody>
-                <?php $counter = 0 ?>
-                <?php foreach ($researchTable->result() as $row) { ?>
-                    <?php $counter++; ?>
-                    <tr>
-                        <td><?= $counter ?></td>
-                        <td><?= $row->project_title ?></td>
-                        <td><?= $row->academic_year ?></td>
-                        <td><?= $this->Research_model->maskProjClassification($row->project_classification_id) ?></td>
-                        <?php $seeResearchers = base_url() . "AddResearch/seeResearchers/$row->id"; ?>
-                        <td><a href="<?= $seeResearchers ?>">Researchers</a></td>
-                        <td><?= $row->filename ?></td>
-                        <td>
-                            <?php
-                            $delete = base_url() . "AddResearch/deleteResearch/$row->id";
-                            $update = base_url() . "";
-                            ?>
-                                <a href="<?=$delete ?>"><button type="button" class="btn ink-reaction btn-raised btn-danger">DELETE</button></a>
-                                <a href="<?=$update ?>"><button type="button" class="btn ink-reaction btn-raised btn-warning">UPDATE</button></a>
-                        </td>
-                    </tr>
-                <?php } ?>
+                    <?php $counter = 0 ?>
+                    <?php foreach ($researchTable->result() as $row) { ?>
+                        <?php $counter++; ?>
+                        <tr>
+                            <td><?= $counter ?></td>
+                            <td><?= $row->project_title ?></td>
+                            <td><?= $row->academic_year ?></td>
+                            <td><?= $this->Research_model->maskProjClassification($row->project_classification_id) ?></td>
+                            <?php $seeResearchers = base_url() . "AddResearch/seeResearchers/$row->id"; ?>
+                            <td><a href="<?= $seeResearchers ?>" style="color:blue">Researchers</a></td>
+                            <td><?= $row->filename ?></td>
+                            <td>
+                                <?php
+                                $delete = base_url() . "AddResearch/deleteResearch/$row->id";
+                                $update = base_url() . "";
+                                ?>
+                                <a href="<?= $delete ?>"><button type="button" class="btn ink-reaction btn-raised btn-danger">DELETE</button></a>
+                                <a href="<?= $update ?>"><button type="button" class="btn ink-reaction btn-raised btn-warning">UPDATE</button></a>
+                            </td>
+                        </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
---
+++ application/views/viewFile.php
@@ -0,0 +1,3 @@
+<h1>View file click here!</h1>
+<?php $url = base_url() . "documents/Vc500 Club.pptx" ?>
+<a href="<?= $url ?>"><button>Click here!</button></a>
